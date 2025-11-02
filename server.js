967e4fcc6f9df2fcc17d0a312dbe476170b9e9be
Adil Mushtaq Dar <115448468+Aadil19@users.noreply.github.com>
2025-11-02 20:26:16 +0530

    updated cors origin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: "https://feedback-frontend-flax.vercel.app/",
+    origin: "https://feedback-frontend-flax.vercel.app",
     methods: ["GET", "POST"],
   })
 );
